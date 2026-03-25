4397b8f7c6ac751f406941c58b731c7578ac68b1
Mohammed Faizan <mdfaizanansari1187@gmail.com>
2026-03-25 14:06:15 +0530

    Hello+Multiple cmd

--- UC6.java
+++ UC6.java
@@ -1,22 +1,60 @@
 public class UC6 {
+
     public static void main(String[] args) {
 
-        // Check if no arguments are provided
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            String result = "Hello, ";
+        // Build banner using helper methods
+        String[] banner = {
+                String.join("  ", getO()[0], getO()[0], getP()[0], getS()[0]),
+                String.join("  ", getO()[1], getO()[1], getP()[1], getS()[1]),
+                String.join("  ", getO()[2], getO()[2], getP()[2], getS()[2]),
+                String.join("  ", getO()[3], getO()[3], getP()[3], getS()[3]),
+                String.join("  ", getO()[4], getO()[4], getP()[4], getS()[4]),
+                String.join("  ", getO()[5], getO()[5], getP()[5], getS()[5]),
+                String.join("  ", getO()[6], getO()[6], getP()[6], getS()[6])
+        };
 
-            // Using enhanced for loop to append names
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-            // Remove trailing comma and space using substring
-            result = result.substring(0, result.length() - 2);
-
-            // Print final greeting
-            System.out.println(result + "!");
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
         }
+    }
+
+    // Helper method for 'O'
+    public static String[] getO() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Helper method for 'P'
+    public static String[] getP() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Helper method for 'S'
+    public static String[] getS() {
+        return new String[] {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
